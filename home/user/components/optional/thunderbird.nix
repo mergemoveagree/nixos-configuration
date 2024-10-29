56db98f2b97408a7c1c640ccb2e3ac156ebd2ca9
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2024-10-29 18:44:39 -0500

    Add openpgp aliases for domain

--- home/user/components/optional/thunderbird.nix
+++ home/user/components/optional/thunderbird.nix
@@ -1,11 +1,32 @@
-{
+{ config
+, ...
+}: {
   programs.thunderbird = {
     enable = true;
     profiles."Main" = {
       isDefault = true;
       withExternalGnupg = true;
+      settings = {
+        "mail.openpgp.fetch_pubkeys_from_gnupg" = true;
+        "mail.openpgp.alias_rules_file" = "file://${config.xdg.configHome}/thunderbird_pgp_aliases.json";
+      };
     };
   };
+
+  xdg.configFile."thunderbird_pgp_aliases.json".text = ''
+    {
+      "rules": [
+        {
+          "domain": "jaden.rs",
+          "keys": [
+            {
+              "fingerprint": "26A987650243B28802524E2F809FD0D502E2F695"
+            }
+          ]
+        }
+      ]
+    }
+  '';
 
   accounts.email.accounts."main".thunderbird = {
     enable = true;
